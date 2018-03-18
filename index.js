652d94f69baeb35529810595127a4742961e67eb
Ajit Fawade <ajitfawade@gmail.com>
2018-03-18 18:08:20 +0530

    added express js and ejs modules.

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const request = require('request');
+/*const argv = require('yargs').argv;*/
+const express = require('express')
+const app = express();
+const apiKey = '4b6b39dc8f6441d9486ff566b5d0e940';
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let apiKey = '4b6b39dc8f6441d9486ff566b5d0e940';
-let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;	
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-request(url, function(err, response, body) {
-	if(err) {
-		console.log('error', err);
-	} else {
-		console.log('body', body);
-	}
+app.get('/', function(req, res) {
+	res.render('index', {weather: null, error: null});
+});
+
+app.post('/', function(req, res) {
+	let city = req.body.city;
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+	request(url, function(err, response, body) {
+		if(err){
+      		res.render('index', {weather: null, error: 'Error, please try again'});
+		} else {
+			let weather = JSON.parse(body);
+			console.log(weather);
+			if(weather.main == undefined){
+			    res.render('index', { weather: null, error: 'Error, please try again' });
+			} else {
+			    let weatherText = `It's ${weather.main.temp} celsius in ${weather.name}!`;
+			    res.render('index', { weather: weatherText, error: null });
+		  	}
+		}
+			
+	});
+});
+
+app.listen(3000, function() {
+	console.log('weatherapp listening on 3000!');
 });
